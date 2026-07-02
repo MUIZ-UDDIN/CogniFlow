7c5426e1e7ca5a73fe746e03fa88cf0aca15055b
MUIZ UD DIN <mueezgamings@gmail.com>
2026-07-02 21:19:04 +0500

    .

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
 const geistMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
